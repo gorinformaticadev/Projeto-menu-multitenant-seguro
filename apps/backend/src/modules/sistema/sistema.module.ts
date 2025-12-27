3811b028cd684f6035ce8a932366b97527d6c940
gorinformaticadev <167313468+gorinformaticadev@users.noreply.github.com>
2025-12-27 10:00:30 -0300

    REmoção de modulos

--- apps/backend/src/modules/sistema/sistema.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SistemaConfigController } from './controller';
-import { SistemaCronService } from './cron.service';
-
-@Module({
-    controllers: [SistemaConfigController],
-    providers: [SistemaCronService],
-})
-export class SistemaModule { }
