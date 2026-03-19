a50cbe5b1cac7740107e6f97e4ab53a366444fe9
gorinformaticadev <167313468+gorinformaticadev@users.noreply.github.com>
2026-03-19 14:23:49 -0300

    Update Sidebar.tsx

--- apps/frontend/src/core/Sidebar.tsx
+++ apps/frontend/src/core/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useAuth } from "@/contexts/AuthContext";
